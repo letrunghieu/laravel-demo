09219839278b7a6c16b9e8d97d3c297770a366b0
trunghieule <letrunghieu.cse09@gmail.com>
2014-07-30 20:13:34 +0700

    Solve the problems :)

--- app/views/errors.blade.php
+++ app/views/errors.blade.php
@@ -8,10 +8,10 @@
 			Error: {{$code}}
 		</h1>
 		<ul class="nav navbar-nav nav-left">
-			<li class="{{ Active::pattern('/') }}"><a href="/">{{ _('Home')}}</a></li>
-			<li class="{{ Active::pattern('about') }}"><a href="{{ url('about') }}">{{ _('About') }}</a></li>
-			<li class="{{ Active::pattern('contact') }}"><a href="{{ url('contact') }}">{{ _('Contact') }}</a></li>
-			<li class="{{ Active::pattern('testimoni') }}"><a href="{{ url('testimoni') }}">{{ _('Testimony') }}</a></li>
+			<li class="{{ Active::pattern('/') }}"><a href="/">{{ trans('Home')}}</a></li>
+			<li class="{{ Active::pattern('about') }}"><a href="{{ url('about') }}">{{ trans('About') }}</a></li>
+			<li class="{{ Active::pattern('contact') }}"><a href="{{ url('contact') }}">{{ trans('Contact') }}</a></li>
+			<li class="{{ Active::pattern('testimoni') }}"><a href="{{ url('testimoni') }}">{{ trans('Testimony') }}</a></li>
 		</ul>
 	</body>
 </html>
