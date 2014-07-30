f1ac0a738ad0ea0f36080edb864f75e3af75f9d5
trunghieule <letrunghieu.cse09@gmail.com>
2014-07-30 12:13:34 +0700

    Add 'active' package

---
+++ app/database/migrations/2014_07_30_045035_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('username');
+			$table->string('password');
+			$table->timestamps();
+		});
+		User::unguard();
+		User::create(['username' => 'johndoe', 'password' => Hash::make('111111')]);
+		User::create(['username' => 'dragonfire', 'password' => Hash::make('111111')]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
